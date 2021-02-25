1053543d01202d1501031928aba76845a20264e7
viktor vaughn <dereck8187@gmail.com>
2021-02-25 14:27:55 -0500

    eomployees and roles added

---
+++ public/index.js
@@ -0,0 +1,54 @@
+const { prompt } = require("inquirer");
+const logo = require("asciiart-logo");
+const db = require("./db");
+require("console.table");
+
+init();
+
+function init() {
+    const logoText = logo({ name: "Employee Manager" }).render();
+  
+    console.log(logoText);
+  
+    loadMainPrompts();
+  }
+  async function loadMainPrompts() {
+    const { choice } = await prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "Make a selection",
+        choices: [
+          {
+            name: "View All Employees",
+            value: "VIEW_EMPLOYEES"
+          },
+          {
+            name: "View All Employees By Department",
+            value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
+          },
+          {
+            name: "Add Employee",
+            value: "ADD_EMPLOYEE"
+          },
+          {
+            name: "Remove Employee",
+            value: "REMOVE_EMPLOYEE"
+          },
+          {
+            name: "Update Employee Role",
+            value: "UPDATE_EMPLOYEE_ROLE"
+          },
+          {
+            name: "View All Roles",
+            value: "VIEW_ROLES"
+          },
+          {
+            name: "Add Role",
+            value: "ADD_ROLE"
+          },
+          {
+            name: "Remove Role",
+            value: "REMOVE_ROLE"
+          },
+        
