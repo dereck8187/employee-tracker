23f1faf5f751f4b19553515fa3151c95b1388b9f
viktor vaughn <dereck8187@gmail.com>
2021-02-25 23:02:33 -0500

    redone

--- db/schema.sql
+++ db/schema.sql
@@ -25,7 +25,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     FOREIGN KEY (role_id) REFERENCES role(id),
-   
+
     PRIMARY KEY (id)
 );
 
@@ -38,4 +38,3 @@ INSERT INTO employee (first_name, last_name, role_id) VALUES ('Jahn', 'Jones', 2
 select employee.id as ID, employee.first_name as First, employee.last_name as Last, department.name as Department, department.id as Dept_ID, role.title as Role, role.id as Role_ID, role.salary as Salary,
 inner join role on employee.role_id = role.id
 inner join department on role.department_id = department.id;
-
--- db/seed.sql
+++ db/seed.sql
@@ -1,24 +1,13 @@
-use employees;
-
 INSERT INTO employee
     (first_name, last_name, role_id)
 VALUES
+    ('Jahn', 'Jones', 1, NULL),
+    ('Addison', 'Montgomery', 2, 1),
     ('Jahn', 'Jones', 2, NULL),
     ('Addison', 'Montgomery', 1),
     ('Richard', 'Webber', 3, NULL),
+    ('Meredith', 'Grey', 4, 3),
     ('Meredith', 'Grey', 2, ),
 
     INSERT INTO department
     (name)
-VALUES
-    ('Surgeon'),
-    ('GP'),
-    ('Nurse'),
-    
-    INSERT INTO role
-    (title, salary, department_id)
-VALUES
-    ('Surgeon', 300000, 1),
-    ('GP', 175000, 2,
-    ('Nurse', 70000, 3),
-    
